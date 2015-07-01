280564ab76ea5fa2ce65b71db03cc8132a11f640
tomcha <tomcha@tomcha.net>
2015-07-01 12:38:32 +0900

    テストコード整理

--- lib/logwriter.rb
+++ lib/logwriter.rb
@@ -1,5 +1,40 @@
 require "logwriter/version"
 
 module Logwriter
-  # Your code goes here...
+  class Logger
+    def initialize(logfile)
+      if File.exist?(logfile)
+        File.open(logfile, 'a') do |io|
+          io.print "\n\n"
+          day = Time.now
+          io.print "#{day.inspect}\n"
+        end
+      else
+        File.open(logfile, 'w') do |io|
+          day = Time.now
+          io.print "#{day.inspect}\n"
+        end
+      end
+      @logfile = logfile
+    end
+
+    def rec_val(target, valiable_name = 'value')
+      File.open(@logfile, 'a') do |io|
+        io.print "#{valiable_name}: "
+        io.print "#{target.inspect}\n"
+      end
+    end
+
+    def rec_class(target)
+      File.open(@logfile, 'a') do |io|
+        io.puts "objectname: #{target.to_str}, class: #{target.class.to_str}, super_class: #{target.super.class.to_str}"
+      end
+    end
+
+    def put_flag(flg_number)
+      File.open(@logfile, 'a') do |io|
+        io.puts "/~~  flaged  #{flg_number}"
+      end
+    end
+  end
 end
--- logwriter.gemspec
+++ logwriter.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["tomcha"]
   spec.email         = ["tomcha@tomcha.net"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{This is a debug helper object class. It makes some log text file.}
+  spec.description   = %Q{This is a debug helper object class. \nIt provides a variety of format of the log output function and it will create any text file.\n}
+  spec.homepage      = "https://github.com/tomcha/logwriter"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
